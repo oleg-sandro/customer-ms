8f569d18a81602f39d3c0f8944dc9fed3b9a9357
Oleg Sandro <example@gmail.com>
2023-07-14 00:18:27 +0300

    BTB-002 Added validation for customer signing up

--- src/main/java/com/besttradebank/investment/customer/controller/CustomerController.java
+++ src/main/java/com/besttradebank/investment/customer/controller/CustomerController.java
@@ -2,19 +2,22 @@ package com.besttradebank.investment.customer.controller;
 
 import com.besttradebank.investment.customer.dto.request.SignUpCustomerRequest;
 import com.besttradebank.investment.customer.dto.response.SignUpCustomerResponse;
+import jakarta.validation.Valid;
 import java.util.UUID;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Validated
 @RestController
 @RequestMapping("/api/v1/customer")
 public class CustomerController {
 
     @PostMapping("/sign-up")
-    public ResponseEntity<SignUpCustomerResponse> signup(@RequestBody SignUpCustomerRequest request) {
+    public ResponseEntity<SignUpCustomerResponse> signup(@Valid @RequestBody SignUpCustomerRequest request) {
         SignUpCustomerResponse response =
                 new SignUpCustomerResponse(UUID.randomUUID(), request.getUsername(), request.getEmail());
         return ResponseEntity.ok(response);
--- src/main/java/com/besttradebank/investment/customer/dto/request/SignUpCustomerRequest.java
+++ src/main/java/com/besttradebank/investment/customer/dto/request/SignUpCustomerRequest.java
@@ -1,5 +1,7 @@
 package com.besttradebank.investment.customer.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,8 +11,14 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class SignUpCustomerRequest {
 
+    @Pattern(regexp = "[a-zA-Z0-9_]{4,63}", message = "Username can contain digits, latin letters and underscores." +
+            " Username length has to be between 4 and 63 characters")
     private String username;
+
+    @Email(message = "Email has to be in format email_address@email_domain")
     private String email;
+
+    @Pattern(regexp = ".{8,63}", message = "Password length has to be between 8 and 63 characters")
     private String password;
 
 }
