091941bf9a8214870233093fde10a60ba25efabe
Oleg Sandro <example@gmail.com>
2023-07-13 23:38:38 +0300

    BTB-001 Added Spring Boot

--- src/main/java/com/besttradebank/investment/customer/Application.java
+++ src/main/java/com/besttradebank/investment/customer/Application.java
@@ -1,8 +1,12 @@
 package com.besttradebank.investment.customer;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        // do nothing
+        SpringApplication.run(Application.class, args);
     }
 }
