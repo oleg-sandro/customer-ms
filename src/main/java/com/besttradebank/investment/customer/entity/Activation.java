ab8b5e3b731527474afcf40171a28f425782527b
Oleg Sandro <example@gmail.com>
2023-07-14 09:11:16 +0300

    BTB-003 Created entity for activation

---
+++ src/main/java/com/besttradebank/investment/customer/entity/Activation.java
@@ -0,0 +1,43 @@
+package com.besttradebank.investment.customer.entity;
+
+import com.besttradebank.investment.customer.enums.ActivationStatusType;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.time.Instant;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Activation {
+
+    @Id
+    private UUID id;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
+    @Enumerated(EnumType.STRING)
+    private ActivationStatusType status;
+
+    private Instant expiredAt;
+
+    @CreationTimestamp
+    private Instant createdAt;
+
+    @UpdateTimestamp
+    private Instant updatedAt;
+
+}
