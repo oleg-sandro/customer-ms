4a6df0d204e5d5be8a5b465ced21f7c3b1e35f3b
Oleg Sandro <example@gmail.com>
2023-07-14 20:23:32 +0300

    BTB-003 Added activation of customer account

--- src/main/java/com/besttradebank/investment/customer/controller/CustomerController.java
+++ src/main/java/com/besttradebank/investment/customer/controller/CustomerController.java
@@ -9,9 +9,9 @@ import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,7 +29,7 @@ public class CustomerController {
         return ResponseEntity.ok(service.signUp(request));
     }
 
-    @PutMapping("/activate/{activationId}")
+    @GetMapping("/activate/{activationId}")
     public ResponseEntity<ActivateCustomerAccountResponse> activate(@PathVariable UUID activationId) {
         return ResponseEntity.ok(service.activate(activationId));
     }
